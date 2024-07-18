b60226d8f884f849ec951f7b9890bcb4d1b1d6f9
Guifré Puig Ribas <guifreribas@gmail.com>
2024-07-18 11:15:38 +0200

    add primary color theme to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,24 @@
-/** @type {import('tailwindcss').Config} */
+// /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,ts}", "./node_modules/flowbite/**/*.js"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          50: "#effef1",
+          100: "#dbfde0",
+          200: "#b8fac2",
+          300: "#81f494",
+          400: "#43e55d",
+          500: "#1bd039",
+          600: "#10a929",
+          700: "#108524",
+          800: "#126921",
+          900: "#11561e",
+          950: "#03300d",
+        },
+      },
+    },
   },
   plugins: [require("flowbite/plugin")],
 };
