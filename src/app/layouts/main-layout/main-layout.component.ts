ecd03b40c168f1c06dcfc9826e4323f05eb07fdc
Guifré Puig Ribas <guifreribas@gmail.com>
2024-07-24 22:26:32 +0200

    add initFlowbite and getMe on mainLayout

--- src/app/layouts/main-layout/main-layout.component.ts
+++ src/app/layouts/main-layout/main-layout.component.ts
@@ -1,7 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
 import { AsideComponent } from '../../components/aside/aside.component';
 import { RouterOutlet } from '@angular/router';
+import { UserService } from '../../services/api_services/user.service';
+import { UserStateService } from '../../services/global_states/user-state.service';
+import { initFlowbite } from 'flowbite';
 
 @Component({
   selector: 'app-main-layout',
@@ -10,4 +13,22 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './main-layout.component.html',
   styleUrl: './main-layout.component.scss',
 })
-export class MainLayoutComponent {}
+export class MainLayoutComponent implements OnInit {
+  private userState = inject(UserStateService);
+  private UserService = inject(UserService);
+
+  ngOnInit(): void {
+    initFlowbite();
+    if (!this.userState.me()) {
+      this.UserService.getMe().subscribe({
+        next: (res) => {
+          console.log({ res });
+          this.userState.setMe(res.data);
+        },
+        error: (err) => {
+          console.log(err);
+        },
+      });
+    }
+  }
+}
