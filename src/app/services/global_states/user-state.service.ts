7b1ac69e81531854afb9e41a311c9e5e56ccd8ad
Guifré Puig Ribas <guifreribas@gmail.com>
2024-07-24 22:25:32 +0200

    add user-state service

---
+++ src/app/services/global_states/user-state.service.ts
@@ -0,0 +1,26 @@
+import { Injectable, signal } from '@angular/core';
+import { User } from '../../models/user';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UserStateService {
+  private _me = signal<User | null>(null);
+
+  readonly me = this._me.asReadonly();
+
+  setMe(user: User) {
+    this._me.set(user);
+  }
+
+  updateMe(userUpdate: Partial<User>) {
+    this._me.update((prev) => {
+      if (!prev) return prev;
+      return { ...prev, ...userUpdate };
+    });
+  }
+
+  clearMe() {
+    this._me.set(null);
+  }
+}
