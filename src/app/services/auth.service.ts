203d467bc3135cd26e77e43b1509155fc018740a
Guifré Puig Ribas <guifreribas@gmail.com>
2024-07-24 07:53:25 +0200

    fix: url auth service and add isLoggedIn

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,22 +1,33 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment.development';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
   private http = inject(HttpClient);
+  private isLoggedIn = false;
+  private url = environment.api_url;
 
   login(email: string, password: string): Observable<any> {
-    return this.http.post('/api/auth/login', { email, password });
+    return this.http.post(`${this.url}/auth/login`, { email, password });
   }
 
   register(email: string, password: string): Observable<any> {
-    return this.http.post('/api/auth/register', { email, password });
+    return this.http.post(`${this.url}/auth/register`, { email, password });
   }
 
-  // logout() {
-  //   return this.http.post('/api/auth/logout');
-  // }
+  logout() {
+    return this.http.delete(`${this.url}/auth/logout`);
+  }
+
+  setLoggedIn(isLoggedIn: boolean) {
+    this.isLoggedIn = isLoggedIn;
+  }
+
+  isAuthenticated(): boolean {
+    return this.isLoggedIn;
+  }
 }
