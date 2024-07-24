bc29f3a983f1a8f7aaf2b5511775db853d3ca677
Guifré Puig Ribas <guifreribas@gmail.com>
2024-07-24 22:27:41 +0200

    feat: navbar

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { AuthService } from '../../services/api_services/auth.service';
+import { tap } from 'rxjs';
+import { AvatarComponent } from '../avatar/avatar.component';
+import { UserStateService } from '../../services/global_states/user-state.service';
+import { User } from '../../models/user';
+import { config } from '../../config/config';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [AvatarComponent],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.scss'
+  styleUrl: './navbar.component.scss',
 })
 export class NavbarComponent {
+  private authService = inject(AuthService);
+  private userState = inject(UserStateService);
 
+  public me = this.userState.me;
+  public imgUrl = config.IMG_URL;
+
+  ngOnInit(): void {
+    console.log(this.imgUrl);
+  }
+
+  logout() {
+    this.authService.logout().subscribe({
+      next: () => {
+        localStorage.removeItem('loggedIn');
+        this.authService.setLoggedIn(false);
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
+  }
 }
