87e770f2d733c2ec0e0cd20d8ab3f8938ab19b43
Guifré Puig Ribas <guifreribas@gmail.com>
2024-07-24 07:57:17 +0200

    feat: add routes and MainLayoutComponent on routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,19 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
+import { MainLayoutComponent } from './layouts/main-layout/main-layout.component';
+import { authGuard } from './auth.guard';
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: '**', redirectTo: '' },
+
+  {
+    path: '',
+    component: MainLayoutComponent,
+    children: [
+      { path: '', component: HomeComponent, canActivate: [authGuard] },
+      { path: '**', redirectTo: '' },
+    ],
+  },
 ];
