fcb4034f11525a7f3ac9b30c17c350615af35845
Guifré Puig Ribas <guifreribas@gmail.com>
2024-07-29 12:30:50 +0200

    feat: add organization card component

---
+++ src/app/components/organization-card/organization-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-organization-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './organization-card.component.html',
+  styleUrl: './organization-card.component.scss'
+})
+export class OrganizationCardComponent {
+
+}
