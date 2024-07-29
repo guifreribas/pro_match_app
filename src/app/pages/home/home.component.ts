6fc28e7af6ce4524d7cd294598c71788a2844485
Guifré Puig Ribas <guifreribas@gmail.com>
2024-07-29 12:34:23 +0200

    refactor: add organization card component on home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { OrganizationCardComponent } from '../../components/organization-card/organization-card.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [OrganizationCardComponent],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
-export class HomeComponent {
-
-}
+export class HomeComponent {}
