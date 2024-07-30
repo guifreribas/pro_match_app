6fdc4b8238a4329d7c8c712813524301685b55b0
Guifré Puig Ribas <guifreribas@gmail.com>
2024-07-30 12:56:23 +0200

    feat: add player card on home page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { OrganizationCardComponent } from '../../components/organization-card/organization-card.component';
+import { PlayerCardComponent } from '../../components/player-card/player-card.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [OrganizationCardComponent],
+  imports: [OrganizationCardComponent, PlayerCardComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
