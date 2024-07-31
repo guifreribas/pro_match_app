6d7c636a28619eaa25e72ec36660b28b80ea3b11
Guifré Puig Ribas <guifreribas@gmail.com>
2024-07-31 12:53:06 +0200

    add dashboard panel layout on home page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
 import { OrganizationCardComponent } from '../../components/organization-card/organization-card.component';
 import { PlayerCardComponent } from '../../components/player-card/player-card.component';
+import { DashboardPanelLayoutComponent } from '../../layouts/dashboard-panel-layout/dashboard-panel-layout.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [OrganizationCardComponent, PlayerCardComponent],
+  imports: [
+    OrganizationCardComponent,
+    PlayerCardComponent,
+    DashboardPanelLayoutComponent,
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
