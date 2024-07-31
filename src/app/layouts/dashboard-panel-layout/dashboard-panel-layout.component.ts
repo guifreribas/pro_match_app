338cd1e733bd0d122340889c8c1a4f1443508bce
Guifré Puig Ribas <guifreribas@gmail.com>
2024-07-31 12:52:11 +0200

    feat: creat dashboard panel layout component

---
+++ src/app/layouts/dashboard-panel-layout/dashboard-panel-layout.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-panel-layout',
+  standalone: true,
+  imports: [],
+  templateUrl: './dashboard-panel-layout.component.html',
+  styleUrl: './dashboard-panel-layout.component.scss'
+})
+export class DashboardPanelLayoutComponent {
+
+}
