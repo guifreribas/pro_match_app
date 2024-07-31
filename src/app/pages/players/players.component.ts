c112e39ff04edf76f675be3fd9d364c3bce6de1e
Guifré Puig Ribas <guifreribas@gmail.com>
2024-07-31 12:53:13 +0200

    add dashboard panel layout on players page

--- src/app/pages/players/players.component.ts
+++ src/app/pages/players/players.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { DashboardPanelLayoutComponent } from '../../layouts/dashboard-panel-layout/dashboard-panel-layout.component';
 
 @Component({
   selector: 'app-players',
   standalone: true,
-  imports: [],
+  imports: [DashboardPanelLayoutComponent],
   templateUrl: './players.component.html',
-  styleUrl: './players.component.scss'
+  styleUrl: './players.component.scss',
 })
-export class PlayersComponent {
-
-}
+export class PlayersComponent {}
