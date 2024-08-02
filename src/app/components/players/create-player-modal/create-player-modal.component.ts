d37639f1866bc19b3612e466de676d27d0b92dac
Guifré Puig Ribas <guifreribas@gmail.com>
2024-08-02 22:55:19 +0200

    feat: added create player modal

---
+++ src/app/components/players/create-player-modal/create-player-modal.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-player-modal',
+  standalone: true,
+  imports: [],
+  templateUrl: './create-player-modal.component.html',
+  styleUrl: './create-player-modal.component.scss'
+})
+export class CreatePlayerModalComponent {
+
+}
