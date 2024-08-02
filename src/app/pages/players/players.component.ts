8112df614ac22c50cdf3a2896a17c4f6a989ea4b
Guifré Puig Ribas <guifreribas@gmail.com>
2024-08-02 22:56:48 +0200

    feat: add players table

--- src/app/pages/players/players.component.ts
+++ src/app/pages/players/players.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { DashboardPanelLayoutComponent } from '../../layouts/dashboard-panel-layout/dashboard-panel-layout.component';
 import { RouterModule } from '@angular/router';
+import { initFlowbite } from 'flowbite';
+import { CreatePlayerModalComponent } from '@app/components/players/create-player-modal/create-player-modal.component';
+import { AsideComponent } from '../../components/organism/aside/aside.component';
 
 @Component({
   selector: 'app-players',
   standalone: true,
-  imports: [DashboardPanelLayoutComponent, RouterModule],
+  imports: [
+    DashboardPanelLayoutComponent,
+    RouterModule,
+    CreatePlayerModalComponent,
+    AsideComponent,
+  ],
   templateUrl: './players.component.html',
   styleUrl: './players.component.scss',
 })
-export class PlayersComponent {}
+export class PlayersComponent implements OnInit {
+  ngOnInit(): void {
+    initFlowbite();
+  }
+}
