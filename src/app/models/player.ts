facc59aa77b4b1ddde6b664949732183321ba3eb
Guifré Puig Ribas <guifreribas@gmail.com>
2024-08-07 17:25:02 +0200

    updated: interfaces on Player model

--- src/app/models/player.ts
+++ src/app/models/player.ts
@@ -3,8 +3,66 @@ export interface Player {
   name: string;
   last_name: string;
   dni: string;
-  avatar: string;
+  avatar?: string;
   birthday: Date;
   created_at?: Date;
   updated_at?: Date;
+}
+
+export interface PlayerGetResponse {
+  success: boolean;
+  message: string;
+  data: Player;
+  timestamp: string;
+}
+
+export interface PlayersGetResponse {
+  success: boolean;
+  message: string;
+  data: {
+    items: Player[];
+    itemCount: number;
+    totalItems: number;
+    currentPage: number;
+    pageSize: number;
+    totalPages: number;
+    hasNextPage: boolean;
+    hasPreviousPage: boolean;
+  };
+  links: {
+    self: string;
+    first: string;
+    last: string;
+    next: string;
+    previous: string;
+  };
+  timestamp: string;
+}
+
+export interface PlayerCreateResponse {
+  success: boolean;
+  message: string;
+  data: Player;
+  timestamp: string;
+}
+
+export interface PlayerUpdateResponse {
+  success: boolean;
+  message: string;
+  data: Player;
+  timestamp: string;
+}
+
+export interface PlayerDeleteResponse {
+  success: boolean;
+  message: string;
+  timestamp: string;
+}
+
+export interface PlayerCreate {
+  name: string;
+  lastName: string;
+  dni: string;
+  avatar?: string;
+  birthday: string;
 }
