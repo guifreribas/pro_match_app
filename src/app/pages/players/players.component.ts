77e683833766652782702bbb55c84f94d7afde34
Guifré Puig Ribas <guifreribas@gmail.com>
2024-08-09 08:51:43 +0200

    feature: get and showed players

--- src/app/pages/players/players.component.ts
+++ src/app/pages/players/players.component.ts
@@ -1,9 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, inject, OnInit } from '@angular/core';
 import { DashboardPanelLayoutComponent } from '../../layouts/dashboard-panel-layout/dashboard-panel-layout.component';
 import { RouterModule } from '@angular/router';
 import { initFlowbite } from 'flowbite';
 import { CreatePlayerModalComponent } from '@app/components/players/create-player-modal/create-player-modal.component';
 import { AsideComponent } from '../../components/organism/aside/aside.component';
+import { PlayerService } from '@app/services/api_services/player.service';
+import { Player, PlayersGetResponse } from '@app/models/player';
 
 @Component({
   selector: 'app-players',
@@ -17,8 +19,29 @@ import { AsideComponent } from '../../components/organism/aside/aside.component'
   templateUrl: './players.component.html',
   styleUrl: './players.component.scss',
 })
-export class PlayersComponent implements OnInit {
+export class PlayersComponent implements OnInit, AfterViewInit {
+  private _playerService = inject(PlayerService);
+  public playersResponse: PlayersGetResponse | null = null;
+  public players: Player[] = [];
+
   ngOnInit(): void {
-    initFlowbite();
+    this._playerService.getPlayers().subscribe({
+      next: (res) => {
+        console.log(res);
+        console.log({ players: res.data.items });
+        this.playersResponse = res;
+        this.players = res.data.items;
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
+  }
+
+  ngAfterViewInit(): void {
+    //Init flowbite after view init to avoid flickering
+    setTimeout(() => {
+      initFlowbite();
+    }, 100);
   }
 }
