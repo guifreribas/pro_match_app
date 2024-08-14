c446e537ac6115ddfc83388abd1ce9edf49db242
Guifré Puig Ribas <guifreribas@gmail.com>
2024-08-14 07:43:17 +0200

    updated: team mode interfaces

--- src/app/models/team.ts
+++ src/app/models/team.ts
@@ -2,7 +2,56 @@ export interface Team {
   id_team: number;
   name: string;
   avatar: string;
-  sport_id: number;
   created_at?: Date;
   updated_at?: Date;
+}
+
+export interface TeamsGetResponse {
+  success: boolean;
+  message: string;
+  data: {
+    items: Team[];
+    itemCount: number;
+    totalItems: number;
+    currentPage: number;
+    pageSize: number;
+    totalPages: number;
+    hasNextPage: boolean;
+    hasPreviousPage: boolean;
+  };
+  links: {
+    self: string;
+    first: string;
+    last: string;
+    next: string;
+    previous: string;
+  };
+  timestamp: string;
+}
+
+export interface TeamGetResponse {
+  success: boolean;
+  message: string;
+  data: Team;
+  timestamp: string;
+}
+
+export interface TeamsCreateResponse {
+  success: boolean;
+  message: string;
+  data: Team;
+  timestamp: string;
+}
+
+export interface TeamUpdateResponse {
+  success: boolean;
+  message: string;
+  data: Team;
+  timestamp: string;
+}
+
+export interface TeamDeleteResponse {
+  success: boolean;
+  message: string;
+  timestamp: string;
 }
