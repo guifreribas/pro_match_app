0cdb42dfa2df62e049d6cc5703a079065687a98a
Guifré Puig Ribas <guifreribas@gmail.com>
2024-08-15 07:23:03 +0200

    feat: added get teams logic

--- src/app/pages/teams/teams.component.ts
+++ src/app/pages/teams/teams.component.ts
@@ -1,11 +1,52 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, inject, OnInit } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { AsideComponent } from '@app/components/organism/aside/aside.component';
+import { CreatePlayerModalComponent } from '@app/components/players/create-player-modal/create-player-modal.component';
+import { CreateTeamModalComponent } from '@app/components/teams/create-team-modal/create-team-modal.component';
 import { DashboardPanelLayoutComponent } from '@app/layouts/dashboard-panel-layout/dashboard-panel-layout.component';
+import { Team, TeamsGetResponse } from '@app/models/team';
+import { TeamService } from '@app/services/api_services/team.service';
+import { initFlowbite } from 'flowbite';
+import dayjs from 'dayjs';
+import { config } from '@app/config/config';
 
 @Component({
   selector: 'app-teams',
   standalone: true,
-  imports: [DashboardPanelLayoutComponent],
+  imports: [
+    DashboardPanelLayoutComponent,
+    RouterModule,
+    CreateTeamModalComponent,
+    AsideComponent,
+  ],
   templateUrl: './teams.component.html',
   styleUrl: './teams.component.scss',
 })
-export class TeamsComponent {}
+export class TeamsComponent implements OnInit, AfterViewInit {
+  private _teamService = inject(TeamService);
+  public teamsResponse: TeamsGetResponse | null = null;
+  public teams: Team[] = [];
+  public createdAt: Date | null = null;
+  public dayjs = dayjs;
+  public IMG_URL = config.IMG_URL;
+
+  ngOnInit(): void {
+    this._teamService.getTeams().subscribe({
+      next: (res) => {
+        console.log(res);
+        console.log({ teams: res.data.items });
+        this.teams = res.data.items;
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
+  }
+
+  ngAfterViewInit(): void {
+    //Init flowbite after view init to avoid flickering
+    setTimeout(() => {
+      initFlowbite();
+    }, 100);
+  }
+}
