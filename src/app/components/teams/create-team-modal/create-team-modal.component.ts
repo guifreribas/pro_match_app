f91791f2a5199962114950df18e9b916a4db320c
Guifré Puig Ribas <guifreribas@gmail.com>
2024-08-15 07:20:14 +0200

    feature: added create team modal with logic

---
+++ src/app/components/teams/create-team-modal/create-team-modal.component.ts
@@ -0,0 +1,107 @@
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { postOneResponse } from '@app/models/api';
+import { ResourceCreateResponse } from '@app/models/resource';
+import { Team, TeamsCreateResponse } from '@app/models/team';
+
+import { GenericApiService } from '@app/services/api_services/generic-api.service';
+import { ResourceService } from '@app/services/api_services/resource.service';
+import { TeamService } from '@app/services/api_services/team.service';
+import { catchError, firstValueFrom } from 'rxjs';
+
+@Component({
+  selector: 'app-create-team-modal',
+  standalone: true,
+  imports: [ReactiveFormsModule, CommonModule, RouterModule],
+  templateUrl: './create-team-modal.component.html',
+  styleUrl: './create-team-modal.component.scss',
+})
+export class CreateTeamModalComponent {
+  public teamForm: FormGroup = new FormGroup({
+    name: new FormControl('', [Validators.required]),
+    avatar: new FormControl('', []),
+  });
+  public isSubmitted = false;
+
+  private _teamService = inject(TeamService);
+  private _resourceService = inject(ResourceService);
+  private _genericService = inject(GenericApiService);
+  private _avatar: File | null = null;
+
+  constructor() {}
+
+  async onSubmit(e: SubmitEvent) {
+    e.preventDefault();
+
+    let team: Team = {
+      ...this.teamForm.value,
+    };
+
+    if (this.teamForm.invalid) {
+      console.log('invalid');
+      return;
+    }
+    let resource: ResourceCreateResponse | null = null;
+    if (this._avatar) {
+      resource = await this.createResource(this._avatar, this._avatar.name);
+      console.log(resource);
+      team.avatar = resource.data.name;
+    }
+
+    const teamCreateResponse = await this.createTeam(team);
+    console.log(teamCreateResponse);
+  }
+
+  async createTeam(team: Team): Promise<postOneResponse<TeamsCreateResponse>> {
+    try {
+      return firstValueFrom(
+        this._genericService
+          .create<Team, postOneResponse<TeamsCreateResponse>>(
+            this._teamService.apiUrl,
+            team
+          )
+          .pipe(
+            catchError((err) => {
+              console.log(err);
+              throw err;
+            })
+          )
+      );
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
+  }
+
+  async createResource(
+    avatar: File,
+    avatarName: string
+  ): Promise<ResourceCreateResponse> {
+    return new Promise((resolve, reject) => {
+      const formData = new FormData();
+      formData.append('file', avatar, avatarName);
+      this._resourceService.createResource(formData).subscribe({
+        next: (res: ResourceCreateResponse) => resolve(res),
+        error: (err) => {
+          console.log(err);
+          reject(err);
+        },
+      });
+    });
+  }
+
+  onAvatarChange(e: any) {
+    if (e?.target?.files) {
+      const file: File = e.target.files[0];
+      console.log(file);
+      this._avatar = file;
+    }
+  }
+}
