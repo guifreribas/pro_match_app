dc225a6d5c12564e3a902a3fa3dc3d42c3ed6822
Guifré Puig Ribas <guifreribas@gmail.com>
2024-08-21 23:05:02 +0200

    feat: add validations for DNI and if player is older than

--- src/app/utils/utils.ts
+++ src/app/utils/utils.ts
@@ -1,3 +1,30 @@
+import { AbstractControl, ValidationErrors } from '@angular/forms';
+
 export const getYears = (birthday: Date) => {
   return new Date().getFullYear() - new Date(birthday).getFullYear();
+};
+
+export const validateDni = (
+  control: AbstractControl
+): ValidationErrors | null => {
+  const dni = control.value;
+  const dniRegex = /^[XYZ\d]\d{7}[A-Z]$/;
+  if (!dniRegex.test(dni)) {
+    return { dni: true };
+  }
+  return null;
+};
+
+export const validatePlayerIsOlderThan = ({
+  birthday,
+  age,
+}: {
+  birthday: string | string[];
+  age: number;
+}) => {
+  const years = getYears(new Date(String(birthday)));
+  if (years < age) {
+    return false;
+  }
+  return true;
 };
