9c52d2d9601e938cc1d4f380492c388c083cd601
Guifré Puig Ribas <guifreribas@gmail.com>
2024-08-23 22:36:01 +0200

    feat: implement player view/edit toggle functionality in player component to switch between viewing and editing states

--- src/app/pages/player/player.component.ts
+++ src/app/pages/player/player.component.ts
@@ -1,26 +1,42 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal, WritableSignal } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { PlayerEditComponent } from '@app/components/player/player-edit/player-edit.component';
+import { PlayerViewComponent } from '@app/components/player/player-view/player-view.component';
+import { config } from '@app/config/config';
 import { DashboardPanelLayoutComponent } from '@app/layouts/dashboard-panel-layout/dashboard-panel-layout.component';
 import { Player } from '@app/models/player';
 import { PlayerService } from '@app/services/api_services/player.service';
 import { getYears } from '@app/utils/utils';
 
+type PlayerViewState = 'VIEW' | 'EDIT';
+
 @Component({
   selector: 'app-player',
   standalone: true,
-  imports: [DashboardPanelLayoutComponent],
+  imports: [
+    DashboardPanelLayoutComponent,
+    PlayerViewComponent,
+    PlayerEditComponent,
+  ],
   templateUrl: './player.component.html',
   styleUrl: './player.component.scss',
 })
 export class PlayerComponent {
+  public imgUrl = config.IMG_URL;
   public player: Player | null = null;
   public playerId: number | null = null;
   public playerYears: number | null = null;
+  public playerViewState: PlayerViewState = 'VIEW';
+  private _isEditing = false;
+
   private _playerService = inject(PlayerService);
   private _route = inject(ActivatedRoute);
 
   ngOnInit(): void {
     this.playerId = this._route.snapshot.params['id'];
+    this._isEditing = this._route.snapshot.queryParams['edit'];
+    if (this._isEditing) this.playerViewState = 'EDIT';
+
     if (this.playerId) {
       this._playerService.getPlayer(this.playerId).subscribe({
         next: (res) => {
