501882dc12fbe8612443efd93af4ea679594a5fd
Guifré Puig Ribas <guifreribas@gmail.com>
2024-08-28 22:01:01 +0200

    feat: enhance player view component with improved layout and team player integration for better user experience and data display

--- src/app/components/player/player-view/player-view.component.ts
+++ src/app/components/player/player-view/player-view.component.ts
@@ -1,8 +1,17 @@
-import { Component, Input, WritableSignal } from '@angular/core';
+import {
+  Component,
+  Input,
+  WritableSignal,
+  OnInit,
+  inject,
+} from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { config } from '@app/config/config';
 import { Player } from '@app/models/player';
+import { TeamPlayer, TeamPlayerWithDetails } from '@app/models/team-player';
+import { TeamPlayerService } from '@app/services/api_services/team-player.service';
 import { PlayerViewState } from '@app/types/player';
+import { signal } from '@angular/core';
 
 @Component({
   selector: 'app-player-view',
@@ -11,9 +20,24 @@ import { PlayerViewState } from '@app/types/player';
   templateUrl: './player-view.component.html',
   styleUrl: './player-view.component.scss',
 })
-export class PlayerViewComponent {
+export class PlayerViewComponent implements OnInit {
   @Input() player!: Player | null;
   @Input() playerYears!: number | null;
   @Input() playerViewState!: WritableSignal<PlayerViewState>;
   public imgUrl: string = config.IMG_URL;
+  public teamPlayers = signal<TeamPlayerWithDetails[]>([]);
+
+  private _teamPlayersService = inject(TeamPlayerService);
+
+  ngOnInit(): void {
+    this._teamPlayersService.getTeamPlayers().subscribe({
+      next: (res) => {
+        console.log(res);
+        this.teamPlayers.set(res.data.items);
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
+  }
 }
