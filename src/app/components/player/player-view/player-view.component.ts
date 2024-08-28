f8d28dc8059044f3bf7c882abd2c077ae189ff0a
Guifré Puig Ribas <guifreribas@gmail.com>
2024-08-28 21:39:57 +0200

    feat: add edit button to player view for triggering edit state and integrate signal for player view state management

--- src/app/components/player/player-view/player-view.component.ts
+++ src/app/components/player/player-view/player-view.component.ts
@@ -1,16 +1,19 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, WritableSignal } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { config } from '@app/config/config';
 import { Player } from '@app/models/player';
+import { PlayerViewState } from '@app/types/player';
 
 @Component({
   selector: 'app-player-view',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './player-view.component.html',
   styleUrl: './player-view.component.scss',
 })
 export class PlayerViewComponent {
   @Input() player!: Player | null;
   @Input() playerYears!: number | null;
+  @Input() playerViewState!: WritableSignal<PlayerViewState>;
   public imgUrl: string = config.IMG_URL;
 }
