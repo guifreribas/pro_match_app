7f1b1f61ef343392639d09d416c1d3757265127f
Guifré Puig Ribas <guifreribas@gmail.com>
2024-08-28 21:11:05 +0200

    feat: wrap team component content in dashboard panel layout for improved styling and structure

--- src/app/pages/team/team.component.ts
+++ src/app/pages/team/team.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { DashboardPanelLayoutComponent } from '@app/layouts/dashboard-panel-layout/dashboard-panel-layout.component';
 
 @Component({
   selector: 'app-team',
   standalone: true,
-  imports: [],
+  imports: [DashboardPanelLayoutComponent],
   templateUrl: './team.component.html',
-  styleUrl: './team.component.scss'
+  styleUrl: './team.component.scss',
 })
-export class TeamComponent {
-
-}
+export class TeamComponent {}
