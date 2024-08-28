bb759e626861661c4a8b629da75612289d6c61af
Guifré Puig Ribas <guifreribas@gmail.com>
2024-08-28 21:11:11 +0200

    fix: update team view template text and integrate DashboardPanelLayoutComponent for improved structure

--- src/app/components/team/team-view/team-view.component.ts
+++ src/app/components/team/team-view/team-view.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { DashboardPanelLayoutComponent } from '../../../layouts/dashboard-panel-layout/dashboard-panel-layout.component';
 
 @Component({
   selector: 'app-team-view',
   standalone: true,
-  imports: [],
+  imports: [DashboardPanelLayoutComponent],
   templateUrl: './team-view.component.html',
-  styleUrl: './team-view.component.scss'
+  styleUrl: './team-view.component.scss',
 })
-export class TeamViewComponent {
-
-}
+export class TeamViewComponent {}
