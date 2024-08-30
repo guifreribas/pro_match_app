f2615c2c38e4ec75d9ecb91f2fb3683ef1f7e114
Guifré Puig Ribas <guifreribas@gmail.com>
2024-08-30 16:46:01 +0200

    feat: implement team edit form with input validation and image upload functionality for enhanced team management features

--- src/app/components/team/team-edit/team-edit.component.ts
+++ src/app/components/team/team-edit/team-edit.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input, WritableSignal } from '@angular/core';
+import { Team } from '@app/models/team';
 
 @Component({
   selector: 'app-team-edit',
   standalone: true,
   imports: [],
   templateUrl: './team-edit.component.html',
-  styleUrl: './team-edit.component.scss'
+  styleUrl: './team-edit.component.scss',
 })
 export class TeamEditComponent {
-
+  @Input() team!: WritableSignal<Team | null>;
 }
