a3bbf170dc9a755f236e6a9e7548c6bf3d3e4b30
Guifré Puig Ribas <guifreribas@gmail.com>
2024-08-30 16:51:16 +0200

    feat: enhance TeamsComponent to fetch teams based on user ID from UserStateService for improved context-aware data retrieval

--- src/app/pages/teams/teams.component.ts
+++ src/app/pages/teams/teams.component.ts
@@ -8,6 +8,7 @@ import { TeamService } from '@app/services/api_services/team.service';
 import { initFlowbite } from 'flowbite';
 import dayjs from 'dayjs';
 import { config } from '@app/config/config';
+import { UserStateService } from '@app/services/global_states/user-state.service';
 
 @Component({
   selector: 'app-teams',
@@ -22,24 +23,29 @@ import { config } from '@app/config/config';
   styleUrl: './teams.component.scss',
 })
 export class TeamsComponent implements OnInit, AfterViewInit {
-  private _teamService = inject(TeamService);
   public teamsResponse: TeamsGetResponse | null = null;
   public teams: Team[] = [];
   public createdAt: Date | null = null;
   public dayjs = dayjs;
   public IMG_URL = config.IMG_URL;
 
+  private _teamService = inject(TeamService);
+  private _userState = inject(UserStateService);
+
   ngOnInit(): void {
-    this._teamService.getTeams().subscribe({
-      next: (res) => {
-        console.log(res);
-        console.log({ teams: res.data.items });
-        this.teams = res.data.items;
-      },
-      error: (err) => {
-        console.log(err);
-      },
-    });
+    console.log('my user id:', this._userState.me()?.id_user);
+    this._teamService
+      .getTeams({ user_id: this._userState.me()?.id_user })
+      .subscribe({
+        next: (res) => {
+          console.log(res);
+          console.log({ teams: res.data.items });
+          this.teams = res.data.items;
+        },
+        error: (err) => {
+          console.log(err);
+        },
+      });
   }
 
   ngAfterViewInit(): void {
