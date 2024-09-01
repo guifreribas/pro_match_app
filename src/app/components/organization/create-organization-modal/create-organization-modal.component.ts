cff7cb4cad7090aa2b5ef01eb56b583c3b3ebd18
Guifré Puig Ribas <guifreribas@gmail.com>
2024-09-01 22:21:05 +0200

    feat: add CreateOrganizationModal component with initial template and styles for organization creation functionality

---
+++ src/app/components/organization/create-organization-modal/create-organization-modal.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-organization-modal',
+  standalone: true,
+  imports: [],
+  templateUrl: './create-organization-modal.component.html',
+  styleUrl: './create-organization-modal.component.scss'
+})
+export class CreateOrganizationModalComponent {
+
+}
