3782a48f3f99e1fae870b60b0586e6e76012153e
Guifré Puig Ribas <guifreribas@gmail.com>
2024-09-01 19:13:06 +0200

    feat: integrate GlobalModalComponent in app.component to enable modal interactions with GlobalModalService for better UI control

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  inject,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { initFlowbite } from 'flowbite';
+import { GlobalModalComponent } from './components/atom/global-modal/global-modal.component';
+import { GlobalModalService } from './services/global-modal.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, GlobalModalComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, AfterViewInit {
+  @ViewChild(GlobalModalComponent) globalModal!: GlobalModalComponent;
   public title = 'pro_match_app';
+
+  private _modalService = inject(GlobalModalService);
+
+  constructor() {}
 
   ngOnInit(): void {
     initFlowbite();
+  }
+
+  ngAfterViewInit(): void {
+    this._modalService.register(this.globalModal);
   }
 }
