7a21b4f005c7a7224a04f5cb51097e83dc6876dc
Guifré Puig Ribas <guifreribas@gmail.com>
2024-09-02 22:20:44 +0200

    feat: add organization component with view/edit logic, HTML template, and SCSS styling for better organization management in app

---
+++ src/app/pages/organization/organization.component.ts
@@ -0,0 +1,45 @@
+import { Component, inject, signal } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { OrganizationEditComponent } from '@app/components/organization/organization-edit/organization-edit.component';
+import { OrganizationViewComponent } from '@app/components/organization/organization-view/organization-view.component';
+import { config } from '@app/config/config';
+import { DashboardPanelLayoutComponent } from '@app/layouts/dashboard-panel-layout/dashboard-panel-layout.component';
+import { Organization } from '@app/models/organization';
+import { OrganizationService } from '@app/services/api_services/organization.service';
+
+@Component({
+  selector: 'app-organization',
+  standalone: true,
+  imports: [
+    DashboardPanelLayoutComponent,
+    OrganizationViewComponent,
+    OrganizationEditComponent,
+  ],
+  templateUrl: './organization.component.html',
+  styleUrl: './organization.component.scss',
+})
+export class OrganizationComponent {
+  public imgUrl = config.IMG_URL;
+  public organizationId: number | null = null;
+  public organization = signal<Organization | null>(null);
+  public organizationViewState = signal<string>('VIEW');
+  public isEditing = false;
+
+  private route = inject(ActivatedRoute);
+  private _organizationService = inject(OrganizationService);
+
+  ngOnInit(): void {
+    this.organizationId = this.route.snapshot.params['id'];
+    if (this.organizationId) {
+      this._organizationService.getOrganization(this.organizationId).subscribe({
+        next: (res) => {
+          console.log(res);
+          this.organization.set(res.data);
+        },
+        error: (err) => {
+          console.log(err);
+        },
+      });
+    }
+  }
+}
