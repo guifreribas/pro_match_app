ad6727e39adee19b9320c778984bc9407028805b
Guifré Puig Ribas <guifreribas@gmail.com>
2024-09-03 10:08:32 +0200

    feat: add gender property to Category interface for better categorization support in the application

--- src/app/models/category.ts
+++ src/app/models/category.ts
@@ -1,6 +1,7 @@
 export interface Category {
   id_category: number;
   name: string;
+  gender: 'MALE' | 'FEMALE' | 'OTHER';
   organization_id: number;
   competition_id: number;
   user_id: number;
