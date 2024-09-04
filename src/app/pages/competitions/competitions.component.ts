e8203b9e7cfff9cb22b5244f9d7c092b2be276d8
Guifré Puig Ribas <guifreribas@gmail.com>
2024-09-04 18:21:44 +0200

    feat: update competition button and modal component for creating competitions with organization data retrieval functionality

--- src/app/pages/competitions/competitions.component.ts
+++ src/app/pages/competitions/competitions.component.ts
@@ -1,24 +1,34 @@
 import { CommonModule } from '@angular/common';
 import { Component, inject, signal } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { CreateCompetitionModalComponent } from '@app/components/competition/create-competition-modal/create-competition-modal.component';
 import { DashboardPanelLayoutComponent } from '@app/layouts/dashboard-panel-layout/dashboard-panel-layout.component';
 import { Competition, CompetitionsGetResponse } from '@app/models/competition';
+import { Organization } from '@app/models/organization';
 import { CompetitionService } from '@app/services/api_services/competition.service';
+import { OrganizationService } from '@app/services/api_services/organization.service';
 import { UserStateService } from '@app/services/global_states/user-state.service';
 import { initFlowbite } from 'flowbite';
 
 @Component({
   selector: 'app-competitions',
   standalone: true,
-  imports: [DashboardPanelLayoutComponent, RouterModule, CommonModule],
+  imports: [
+    DashboardPanelLayoutComponent,
+    CreateCompetitionModalComponent,
+    RouterModule,
+    CommonModule,
+  ],
   templateUrl: './competitions.component.html',
   styleUrl: './competitions.component.scss',
 })
 export class CompetitionsComponent {
   public competitionsResponse = signal<CompetitionsGetResponse | null>(null);
   public competitions: Competition[] = [];
+  public organizations = signal<Organization[]>([]);
 
   private _competitionService = inject(CompetitionService);
+  private _organizationService = inject(OrganizationService);
   private _userState = inject(UserStateService);
 
   constructor() {}
@@ -41,5 +51,19 @@ export class CompetitionsComponent {
     setTimeout(() => {
       initFlowbite();
     }, 100);
+  }
+
+  onCreateCompetitionModal() {
+    this._organizationService
+      .getOrganizations({ user_id: this._userState.me()!.id_user })
+      .subscribe({
+        next: (res) => {
+          console.log('organizations', res);
+          this.organizations.set(res.data.items);
+        },
+        error: (err) => {
+          console.log(err);
+        },
+      });
   }
 }
