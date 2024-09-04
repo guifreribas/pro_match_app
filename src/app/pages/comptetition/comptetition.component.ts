ded77c8840a24d240cb58c1398e01aba29aed020
Guifré Puig Ribas <guifreribas@gmail.com>
2024-09-04 18:21:51 +0200

    feat: create competition component with basic HTML structure and SCSS styles for initial setup

---
+++ src/app/pages/comptetition/comptetition.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-comptetition',
+  standalone: true,
+  imports: [],
+  templateUrl: './comptetition.component.html',
+  styleUrl: './comptetition.component.scss'
+})
+export class ComptetitionComponent {
+
+}
