2a402557f497348bf5789a7577651cd2bfdb4190
Guifré Puig Ribas <guifreribas@gmail.com>
2024-09-06 10:28:53 +0200

    Refactor: Introduce gender mapping and responsive popover position handling in CompetitionsComponent for better user experience

--- src/app/pages/competitions/competitions.component.ts
+++ src/app/pages/competitions/competitions.component.ts
@@ -1,9 +1,14 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject, signal } from '@angular/core';
+import { Component, HostListener, inject, signal } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { CreateCompetitionModalComponent } from '@app/components/competition/create-competition-modal/create-competition-modal.component';
+import { CompetitionTypes } from '@app/config/constants';
 import { DashboardPanelLayoutComponent } from '@app/layouts/dashboard-panel-layout/dashboard-panel-layout.component';
-import { Competition, CompetitionsGetResponse } from '@app/models/competition';
+import {
+  Competition,
+  CompetitionsGetResponse,
+  CompetitionWithDetails,
+} from '@app/models/competition';
 import { Organization } from '@app/models/organization';
 import { CompetitionService } from '@app/services/api_services/competition.service';
 import { OrganizationService } from '@app/services/api_services/organization.service';
@@ -23,28 +28,53 @@ import { initFlowbite } from 'flowbite';
   styleUrl: './competitions.component.scss',
 })
 export class CompetitionsComponent {
+  public popoverPosition = signal('bottom');
   public competitionsResponse = signal<CompetitionsGetResponse | null>(null);
-  public competitions: Competition[] = [];
+  public competitions: CompetitionWithDetails[] = [];
   public organizations = signal<Organization[]>([]);
+  public competitionType = {
+    LEAGUE: 'Liga',
+    TOURNAMENT: 'Copa',
+  };
+
+  public competitionFormatMap = {
+    SINGLE_ROUND: 'Sólo ida',
+    DOUBLE_ROUND: 'Doble vuelta',
+    KNOCKOUT: 'Eliminatorias',
+    KNOCKOUT_SINGLE_ROUND: 'Liguilla una vuelta más eliminatorias',
+    KNOCKOUT_DOUBLE_ROUND: 'Liguilla dos vueltas más eliminatorias',
+  };
 
   private _competitionService = inject(CompetitionService);
   private _organizationService = inject(OrganizationService);
   private _userState = inject(UserStateService);
+  private _genderMap: { [key: string]: string } = {
+    MALE: 'Masculino',
+    FEMALE: 'Femenino',
+    OTHER: 'Otro',
+  };
 
   constructor() {}
 
   ngOnInit(): void {
-    this._competitionService.getCompetitions().subscribe({
-      next: (res) => {
-        console.log(res);
-        console.log({ competitions: res.data.items });
-        this.competitionsResponse.set(res);
-        this.competitions = res.data.items;
-      },
-      error: (err) => {
-        console.log(err);
-      },
-    });
+    this._competitionService
+      .getCompetitions({
+        user_id: this._userState.me()!.id_user,
+        includeCompetitionType: true,
+        includeOrganization: true,
+        includeCompetitionCategory: true,
+      })
+      .subscribe({
+        next: (res) => {
+          console.log(res);
+          console.log({ competitions: res.data.items });
+          this.competitionsResponse.set(res);
+          this.competitions = res.data.items;
+        },
+        error: (err) => {
+          console.log(err);
+        },
+      });
   }
 
   ngAfterViewInit(): void {
@@ -55,7 +85,9 @@ export class CompetitionsComponent {
 
   onCreateCompetitionModal() {
     this._organizationService
-      .getOrganizations({ user_id: this._userState.me()!.id_user })
+      .getOrganizations({
+        user_id: this._userState.me()!.id_user,
+      })
       .subscribe({
         next: (res) => {
           console.log('organizations', res);
@@ -65,5 +97,20 @@ export class CompetitionsComponent {
           console.log(err);
         },
       });
+  }
+
+  getGender(gender: string = '') {
+    return this._genderMap[gender] || 'Otro';
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event?: Event): void {
+    const screenWidth = window.innerWidth;
+    const isSmallScreen = screenWidth < 768;
+    if (isSmallScreen) {
+      this.popoverPosition.set('bottom');
+    } else {
+      this.popoverPosition.set('left');
+    }
   }
 }
