da237c5154a67b093877700790fa2ff0e5905af3
Guifré Puig Ribas <guifreribas@gmail.com>
2024-09-06 19:53:40 +0200

    feat: Implement competition edit component with basic HTML template and initial SCSS file for styling

---
+++ src/app/components/competition/competition-edit/competition-edit.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-competition-edit',
+  standalone: true,
+  imports: [],
+  templateUrl: './competition-edit.component.html',
+  styleUrl: './competition-edit.component.scss'
+})
+export class CompetitionEditComponent {
+
+}
