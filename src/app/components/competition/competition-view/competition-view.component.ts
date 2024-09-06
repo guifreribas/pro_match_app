7fa7faac6af983e3ba8a94c19a648066d136dd14
Guifré Puig Ribas <guifreribas@gmail.com>
2024-09-06 19:53:52 +0200

    feat: Create CompetitionView component with initial HTML template and empty SCSS file for future enhancements

---
+++ src/app/components/competition/competition-view/competition-view.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-competition-view',
+  standalone: true,
+  imports: [],
+  templateUrl: './competition-view.component.html',
+  styleUrl: './competition-view.component.scss'
+})
+export class CompetitionViewComponent {
+
+}
