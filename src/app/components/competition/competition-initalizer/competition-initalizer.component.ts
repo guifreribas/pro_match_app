9c48276101cc9e7896efad3dcea52115fb1dd070
Guifré Puig Ribas <guifreribas@gmail.com>
2024-09-06 19:53:46 +0200

    feat: Add CompetitionInitializer component with initial HTML template and empty SCSS file for future styling improvements

---
+++ src/app/components/competition/competition-initalizer/competition-initalizer.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-competition-initalizer',
+  standalone: true,
+  imports: [],
+  templateUrl: './competition-initalizer.component.html',
+  styleUrl: './competition-initalizer.component.scss'
+})
+export class CompetitionInitalizerComponent {
+
+}
