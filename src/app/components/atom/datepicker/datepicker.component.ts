d8c29444683a1eb7e7dca0004c1d6f8160257a0b
Guifré Puig Ribas <guifreribas@gmail.com>
2024-09-06 19:46:24 +0200

    style: Update datepicker and input component styles to use darker primary color for consistency across the app's UI

--- src/app/components/atom/datepicker/datepicker.component.ts
+++ src/app/components/atom/datepicker/datepicker.component.ts
@@ -75,7 +75,7 @@ export const MY_DATE_FORMATS = {
         [matDatepicker]="picker"
         [formControl]="dateControl"
         placeholder="Selecciona una data"
-        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
+        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-700 focus:border-primary-700 block w-full p-2.5"
       />
       <mat-datepicker-toggle
         matSuffix
