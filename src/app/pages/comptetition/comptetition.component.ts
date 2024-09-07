9bd8c030f2f4cb2f98bb78b44779fdcc865d6cce
Guifré Puig Ribas <guifreribas@gmail.com>
2024-09-07 22:37:44 +0200

    feat: Refactor CompetitionComponent to include dashboard panel and competition initializer for enhanced layout and functionality

--- src/app/pages/comptetition/comptetition.component.ts
+++ src/app/pages/comptetition/comptetition.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
+import { CompetitionInitalizerComponent } from '@app/components/competition/competition-initalizer/competition-initalizer.component';
+import { DashboardPanelLayoutComponent } from '@app/layouts/dashboard-panel-layout/dashboard-panel-layout.component';
 
 @Component({
   selector: 'app-comptetition',
   standalone: true,
-  imports: [],
+  imports: [DashboardPanelLayoutComponent, CompetitionInitalizerComponent],
   templateUrl: './comptetition.component.html',
-  styleUrl: './comptetition.component.scss'
+  styleUrl: './comptetition.component.scss',
 })
-export class ComptetitionComponent {
-
-}
+export class ComptetitionComponent {}
