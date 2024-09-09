4048ae1b46b868bbc2d621a105ee0611ad113014
Guifré Puig Ribas <guifreribas@gmail.com>
2024-09-09 02:03:40 +0200

    feat: Implement competition initializer with search functionality and date selection for enhanced event setup flow

--- src/app/components/competition/competition-initalizer/competition-initalizer.component.ts
+++ src/app/components/competition/competition-initalizer/competition-initalizer.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit, signal } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
+import { DatepickerComponent } from '@app/components/atom/datepicker/datepicker.component';
 
 @Component({
   selector: 'app-competition-initalizer',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule, RouterLink, DatepickerComponent],
   templateUrl: './competition-initalizer.component.html',
-  styleUrl: './competition-initalizer.component.scss'
+  styleUrl: './competition-initalizer.component.scss',
 })
-export class CompetitionInitalizerComponent {
+export class CompetitionInitalizerComponent implements OnInit {
+  public competitionSearchInput: FormControl = new FormControl('');
+  public searchedPlayers = signal<any[] | null>(null);
+  public dynamicClasses: { [key: string]: boolean } = {};
 
+  ngOnInit(): void {}
+
+  onSearchInput(e: Event) {}
+  getDorsalControl(playerId: number): FormControl {
+    return new FormControl('');
+  }
+
+  isInvalidDorsal(playerId: number): boolean {
+    return false;
+  }
+
+  handleAddPlayer(playerId: number) {}
+
+  onDateChange(event: any) {}
 }
