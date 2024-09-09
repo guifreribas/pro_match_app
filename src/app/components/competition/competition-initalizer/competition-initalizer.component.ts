77786562f8a92ddceeb4434a3cd24f68b370e56b
Guifré Puig Ribas <guifreribas@gmail.com>
2024-09-09 02:04:09 +0200

    feat: Integrate CompetitionService in CompetitionInitializer for fetching competitions and updating state on init for better data handling

--- src/app/components/competition/competition-initalizer/competition-initalizer.component.ts
+++ src/app/components/competition/competition-initalizer/competition-initalizer.component.ts
@@ -1,8 +1,9 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit, signal } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 import { DatepickerComponent } from '@app/components/atom/datepicker/datepicker.component';
+import { CompetitionService } from '@app/services/api_services/competition.service';
 
 @Component({
   selector: 'app-competition-initalizer',
@@ -16,7 +17,19 @@ export class CompetitionInitalizerComponent implements OnInit {
   public searchedPlayers = signal<any[] | null>(null);
   public dynamicClasses: { [key: string]: boolean } = {};
 
-  ngOnInit(): void {}
+  private _competitionService = inject(CompetitionService);
+
+  ngOnInit(): void {
+    this._competitionService.getCompetitions().subscribe({
+      next: (res) => {
+        console.log(res);
+        this.searchedPlayers.set(res.data.items);
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
+  }
 
   onSearchInput(e: Event) {}
   getDorsalControl(playerId: number): FormControl {
