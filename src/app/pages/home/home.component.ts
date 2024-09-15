2c19c77deb161df11c731705195d3df9930c2d1d
Guifré Puig Ribas <guifreribas@gmail.com>
2024-09-15 03:29:20 +0200

    feat: enhance HomeComponent with overview data fetching and reactive signals for better state management

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,7 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { OrganizationCardComponent } from '../../components/organization-card/organization-card.component';
 import { PlayerCardComponent } from '../../components/player-card/player-card.component';
 import { DashboardPanelLayoutComponent } from '../../layouts/dashboard-panel-layout/dashboard-panel-layout.component';
+import { RouterLink } from '@angular/router';
+import { OverviewService } from '@app/services/api_services/overview.service';
+import { Overview, OverviewGetResponse } from '@app/models/overview';
+import { UserStateService } from '@app/services/global_states/user-state.service';
 
 @Component({
   selector: 'app-home',
@@ -10,8 +14,27 @@ import { DashboardPanelLayoutComponent } from '../../layouts/dashboard-panel-lay
     OrganizationCardComponent,
     PlayerCardComponent,
     DashboardPanelLayoutComponent,
+    RouterLink,
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
-export class HomeComponent {}
+export class HomeComponent implements OnInit {
+  public overviewResponse = signal<OverviewGetResponse | null>(null);
+  public overview = signal<Overview | null>(null);
+  private _overviewService = inject(OverviewService);
+  constructor() {}
+
+  ngOnInit(): void {
+    this._overviewService.getOverview().subscribe({
+      next: (res) => {
+        console.log(res);
+        this.overviewResponse.set(res);
+        this.overview.set(res.data);
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
+  }
+}
