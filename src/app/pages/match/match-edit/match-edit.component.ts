925411fe8eac154d07ed727bd92e633a681fed6c
Guifré Puig Ribas <guifreribas@gmail.com>
2024-09-17 10:55:32 +0200

    feat: create match-edit component with basic template and styles to facilitate match editing functionality in the application

---
+++ src/app/pages/match/match-edit/match-edit.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-match-edit',
+  standalone: true,
+  imports: [],
+  templateUrl: './match-edit.component.html',
+  styleUrl: './match-edit.component.scss'
+})
+export class MatchEditComponent {
+
+}
