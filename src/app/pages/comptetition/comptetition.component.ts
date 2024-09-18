cd5fca1bb76d8bfad3489df6d09f38705a7bc958
Guifré Puig Ribas <guifreribas@gmail.com>
2024-09-18 18:37:57 +0200

    feat: update competition edit component to pass organizations and categories props, integrate data fetching in component logic

--- src/app/pages/comptetition/comptetition.component.ts
+++ src/app/pages/comptetition/comptetition.component.ts
@@ -1,12 +1,20 @@
-import { Component, inject, OnInit, signal } from '@angular/core';
+import { Component, effect, inject, OnInit, signal } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { CompetitionEditComponent } from '@app/components/competition/competition-edit/competition-edit.component';
 import { CompetitionInitalizerComponent } from '@app/components/competition/competition-initalizer/competition-initalizer.component';
 import { CompetitionViewComponent } from '@app/components/competition/competition-view/competition-view.component';
 import { DashboardPanelLayoutComponent } from '@app/layouts/dashboard-panel-layout/dashboard-panel-layout.component';
-import { CompetitionWithDetails } from '@app/models/competition';
+import { Category } from '@app/models/category';
+import {
+  CompetitionsGetResponse,
+  CompetitionWithDetails,
+} from '@app/models/competition';
+import { Organization } from '@app/models/organization';
+import { CategoryService } from '@app/services/api_services/category.service';
 import { CompetitionService } from '@app/services/api_services/competition.service';
+import { OrganizationService } from '@app/services/api_services/organization.service';
 import { CompetitionStateService } from '@app/services/global_states/competition-state.service';
+import { UserStateService } from '@app/services/global_states/user-state.service';
 
 type CompetitionViewState = 'VIEW' | 'EDIT' | 'INITIALIZE';
 
@@ -24,30 +32,105 @@ type CompetitionViewState = 'VIEW' | 'EDIT' | 'INITIALIZE';
 })
 export class ComptetitionComponent implements OnInit {
   public competitionId = '';
-  public competition = signal<CompetitionWithDetails | null>(null);
   public competitionViewState = signal<CompetitionViewState>('VIEW');
+  public competitionsResponse = signal<CompetitionsGetResponse | null>(null);
+  public competitions: CompetitionWithDetails[] = [];
+  public organizations = signal<Organization[]>([]);
+  public categories = signal<Category[]>([]);
 
   private _competitionService = inject(CompetitionService);
   private _competitionState = inject(CompetitionStateService);
+  private _organizationService = inject(OrganizationService);
+  private _categoryService = inject(CategoryService);
+  private _userState = inject(UserStateService);
   private _route = inject(ActivatedRoute);
-  private _isEditing = false;
+  private _hasFetchedCompetitions = false;
+  private _hasFetchedOrganizations = false;
+  private _hasFetchedCategories = false;
+
+  constructor() {
+    const idCompetition = this._route.snapshot.params['id'];
+    effect(() => {
+      const user = this._userState.me();
+      if (user?.id_user && this._hasFetchedCompetitions === false) {
+        this._competitionService
+          .getCompetitions({
+            user_id: user.id_user,
+            includeCompetitionType: true,
+            includeOrganization: true,
+            includeCompetitionCategory: true,
+            id_competition: idCompetition,
+          })
+          .subscribe({
+            next: (res) => {
+              console.log(res);
+              console.log({ competitions: res.data.items });
+              this.competitionsResponse.set(res);
+              this._competitionState.setCompetition(res.data.items[0]);
+              // this.competitions = res.data.items;
+            },
+            error: (err) => {
+              console.log(err);
+            },
+          });
+      }
+      this._hasFetchedCompetitions = true;
+    });
+
+    effect(() => {
+      const user = this._userState.me();
+      if (user?.id_user && this._hasFetchedOrganizations === false) {
+        this._organizationService
+          .getOrganizations({
+            user_id: this._userState.me()!.id_user,
+          })
+          .subscribe({
+            next: (res) => {
+              console.log('organizations Response!', res);
+              this.organizations.set(res.data.items);
+            },
+            error: (err) => {
+              console.log(err);
+            },
+          });
+        this._hasFetchedOrganizations = true;
+      }
+    });
+
+    effect(() => {
+      const user = this._userState.me();
+      if (user?.id_user && this._hasFetchedCategories === false) {
+        this._categoryService
+          .getCategories({ user_id: this._userState.me()!.id_user, limit: 100 })
+          .subscribe({
+            next: (res) => {
+              console.log(res);
+              this.categories.set(res.data.items);
+            },
+            error: (err) => {
+              console.log(err);
+            },
+          });
+        this._hasFetchedCategories = true;
+      }
+    });
+  }
 
   ngOnInit(): void {
-    this.competitionId = this._route.snapshot.params['id'] || '';
     const status = this._route.snapshot.queryParams['status'];
     this.competitionViewState.set(status);
-
-    if (this.competitionId) {
-      this._competitionService
-        .getCompetition(Number(this.competitionId))
-        .subscribe({
-          next: (res) => {
-            this._competitionState.competitions.set(res);
-          },
-          error: (err) => {
-            console.log(err);
-          },
-        });
-    }
+    // this.competitionId = this._route.snapshot.params['id'] || '';
+    // if (this.competitionId) {
+    //   this._competitionService
+    //     .getCompetition(Number(this.competitionId))
+    //     .subscribe({
+    //       next: (res) => {
+    //         this._competitionState.setCompetition(res.data);
+    //       },
+    //       error: (err) => {
+    //         console.log(err);
+    //       },
+    //     });
+    // }
   }
 }
