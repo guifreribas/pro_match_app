dd68479b82f9551eb5ea82131300c8f2d2a6aa50
Guifré Puig Ribas <guifreribas@gmail.com>
2024-09-18 18:37:27 +0200

    feat: update competition model to include competition_type_id and modify categories for enhanced data structure and clarity

--- src/app/models/competition.ts
+++ src/app/models/competition.ts
@@ -10,21 +10,25 @@ export interface Competition {
   name: string;
   format: CompetitionFormat;
   is_initialized: boolean;
-  user_id: number;
+  competition_type_id: number;
   organization_id: number;
+  user_id: number;
   created_at?: Date;
   updated_at?: Date;
 }
 
 interface CompetitionCategoryWithDetails {
   season: string | null;
-  category: Pick<Category, 'name' | 'gender'> | null;
+  category: Pick<Category, 'id_category' | 'name' | 'gender'> | null;
   competition_category_id: number;
 }
 
 export interface CompetitionWithDetails extends Competition {
-  organization: Pick<Organization, 'name' | 'logo' | 'address'> | null;
-  competitionType: Pick<CompetitionType, 'name'> | null;
+  organization: Pick<
+    Organization,
+    'id_organization' | 'name' | 'logo' | 'address'
+  > | null;
+  competitionType: Pick<CompetitionType, 'id_competition_type' | 'name'> | null;
   competitionCategory: CompetitionCategoryWithDetails;
   competition_category_id: Pick<CompetitionCategory, 'id_competition_category'>;
 }
