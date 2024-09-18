bbd872dde0e2233561cb524bbfd0d647925e5e64
Guifré Puig Ribas <guifreribas@gmail.com>
2024-09-18 09:56:23 +0200

    feat: update app config to enable view transitions and add BrowserAnimationsModule for improved animation handling

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,19 +3,24 @@ import {
   importProvidersFrom,
   provideZoneChangeDetection,
 } from '@angular/core';
-import { provideRouter, RouterModule } from '@angular/router';
+import {
+  provideRouter,
+  RouterModule,
+  withViewTransitions,
+} from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { credentialsInterceptor } from './interceptors/credentials.interceptor';
 import { provideNativeDateAdapter } from '@angular/material/core';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
-    importProvidersFrom(RouterModule),
+    provideRouter(routes, withViewTransitions()),
+    importProvidersFrom(RouterModule, BrowserAnimationsModule),
     provideHttpClient(withInterceptors([credentialsInterceptor])),
     provideNativeDateAdapter(),
     provideAnimationsAsync(),
