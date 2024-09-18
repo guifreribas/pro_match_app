b7e70f3521a76c13d500c61dc117bb2e2ae0e4d9
Guifré Puig Ribas <guifreribas@gmail.com>
2024-09-18 09:56:40 +0200

    feat: enhance CompetitionStateService with methods for getting, setting, updating, and clearing competition state management

--- src/app/services/global_states/competition-state.service.ts
+++ src/app/services/global_states/competition-state.service.ts
@@ -1,14 +1,30 @@
 import { Injectable, signal } from '@angular/core';
-import { getOneResponse } from '@app/models/api';
 import { CompetitionWithDetails } from '@app/models/competition';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CompetitionStateService {
-  public competitions = signal<getOneResponse<CompetitionWithDetails> | null>(
-    null
-  );
+  readonly competitions = signal<CompetitionWithDetails | null>(null);
 
   constructor() {}
+
+  getCompetition(): CompetitionWithDetails | null {
+    return this.competitions();
+  }
+
+  setCompetition(competition: CompetitionWithDetails) {
+    this.competitions.set(competition);
+  }
+
+  updateCompetition(competition: Partial<CompetitionWithDetails>) {
+    this.competitions.update((prev) => {
+      if (!prev) return prev;
+      return { ...prev, ...competition };
+    });
+  }
+
+  clearCompetition() {
+    this.competitions.set(null);
+  }
 }
