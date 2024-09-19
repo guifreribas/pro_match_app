257f6a1c2c69689bc3d5c7277b5aaf40455ff075
Guifré Puig Ribas <guifreribas@gmail.com>
2024-09-19 20:47:23 +0200

    feat: create AddFoulComponent with basic HTML template and SCSS styling for foul addition functionality

---
+++ src/app/components/match/add-foul/add-foul.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-foul',
+  standalone: true,
+  imports: [],
+  templateUrl: './add-foul.component.html',
+  styleUrl: './add-foul.component.scss'
+})
+export class AddFoulComponent {
+
+}
