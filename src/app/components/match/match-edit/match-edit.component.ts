80e5602c41d9ffc30127ecfa3614ba69b846ba19
Guifré Puig Ribas <guifreribas@gmail.com>
2024-09-19 20:47:49 +0200

    feat: enhance MatchEditComponent with form toggling logic and integrate AddGoal, AddCard, and AddFoul components for interactions

--- src/app/components/match/match-edit/match-edit.component.ts
+++ src/app/components/match/match-edit/match-edit.component.ts
@@ -1,13 +1,28 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { DashboardPanelLayoutComponent } from '@app/layouts/dashboard-panel-layout/dashboard-panel-layout.component';
 import { MatchService } from '@app/services/api_services/match.service';
+import { AddGoalComponent } from '../add-goal/add-goal.component';
+import { AddCardComponent } from '../add-card/add-card.component';
+import { AddFoulComponent } from '../add-foul/add-foul.component';
+
+type FormType = 'GOAL' | 'CARD' | 'FOUL' | null;
 
 @Component({
   selector: 'app-match-edit',
   standalone: true,
-  imports: [],
+  imports: [AddGoalComponent, AddCardComponent, AddFoulComponent],
   templateUrl: './match-edit.component.html',
   styleUrl: './match-edit.component.scss',
 })
-export class MatchEditComponent {}
+export class MatchEditComponent {
+  public wichFormIsActive = signal<FormType>(null);
+
+  setFormType(formType: FormType) {
+    if (this.wichFormIsActive() === formType) {
+      this.wichFormIsActive.set(null);
+      return;
+    }
+    this.wichFormIsActive.set(formType);
+  }
+}
