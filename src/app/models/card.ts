f67382df722c46ea6f955a804b7f544d381b1960
Guifré Puig Ribas <guifreribas@gmail.com>
2024-09-20 21:29:36 +0200

    feat: make id_card optional and add user_id to Card interface for improved data handling in card management

--- src/app/models/card.ts
+++ src/app/models/card.ts
@@ -7,13 +7,14 @@ export type CardPart =
   | 'PENALTIES';
 
 export interface Card {
-  id_card: number;
+  id_card?: number;
   card_type: CardType;
   minute: number;
   part: CardPart;
   match_id: number;
   player_id: number;
   team_id: number;
+  user_id: number;
   created_at?: Date;
   updated_at?: Date;
 }
