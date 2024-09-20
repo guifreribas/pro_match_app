2363e5265e85cdd939a0541a30146ce6b56e3c58
Guifré Puig Ribas <guifreribas@gmail.com>
2024-09-20 14:29:39 +0200

    feat: enhance MatchStateService with signal handling methods for match data management, including update and clear functionalities

--- src/app/services/global_states/match-state.service.ts
+++ src/app/services/global_states/match-state.service.ts
@@ -1,9 +1,41 @@
-import { Injectable } from '@angular/core';
+import { Injectable, signal } from '@angular/core';
+import { MatchWithDetails } from '@app/models/match';
+import { Team } from '@app/models/team';
+import { TeamPlayerWithDetails } from '@app/models/team-player';
+
+interface MatchCompletedData {
+  match: MatchWithDetails;
+  goals: any[];
+  localTeam: Team;
+  visitorTeam: Team;
+  localPlayers: TeamPlayerWithDetails[];
+  visitorPlayers: TeamPlayerWithDetails[];
+}
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class MatchStateService {
+  public match = signal<MatchCompletedData | null>(null);
 
-  constructor() { }
+  constructor() {}
+
+  getMatch(): any | null {
+    return this.match;
+  }
+
+  setMatch(match: any) {
+    this.match.set(match);
+  }
+
+  updateMatch(match: Partial<any>) {
+    this.match.update((prev) => {
+      if (!prev) return prev;
+      return { ...prev, ...match };
+    });
+  }
+
+  clearMatch() {
+    this.match.set(null);
+  }
 }
