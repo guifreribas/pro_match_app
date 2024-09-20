f90bbdb457e857f96549d9d23a0bbcdda7247e26
Guifré Puig Ribas <guifreribas@gmail.com>
2024-09-20 21:29:26 +0200

    feat: update CardService to accept parameters for getCards, improving API flexibility and response URL generation

--- src/app/services/api_services/card.service.ts
+++ src/app/services/api_services/card.service.ts
@@ -10,6 +10,14 @@ import {
 } from '../../models/api';
 import { Card } from '../../models/card';
 import { GenericApiService } from './generic-api.service';
+import { urlParser } from '@app/utils/utils';
+
+interface GetCardsParams {
+  q: string;
+  page: string;
+  user_id: number;
+  limit: number;
+}
 
 @Injectable({
   providedIn: 'root',
@@ -20,8 +28,9 @@ export class CardService {
 
   constructor() {}
 
-  getCards(): Observable<getAllResponse<Card>> {
-    return this.genericService.getAll<getAllResponse<Card>>(this.apiUrl);
+  getCards(params: Partial<GetCardsParams>): Observable<getAllResponse<Card>> {
+    const url = urlParser(params, this.apiUrl);
+    return this.genericService.getAll<getAllResponse<Card>>(url);
   }
 
   getCard(id: number): Observable<getOneResponse<Card>> {
