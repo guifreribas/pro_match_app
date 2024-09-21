fc20914668668a8cf8a8d338aeb584822cd5c1bc
Guifré Puig Ribas <guifreribas@gmail.com>
2024-09-21 17:43:53 +0200

    feat: update match-edit component to dynamically display team names and goals; integrate star toggle for player selection

--- src/app/components/match/match-edit/match-edit.component.ts
+++ src/app/components/match/match-edit/match-edit.component.ts
@@ -1,18 +1,15 @@
-import { Component, inject, signal } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { DashboardPanelLayoutComponent } from '@app/layouts/dashboard-panel-layout/dashboard-panel-layout.component';
-import { MatchService } from '@app/services/api_services/match.service';
+import { Component, effect, inject, Input, signal } from '@angular/core';
 import { AddGoalComponent } from '../add-goal/add-goal.component';
 import { AddCardComponent } from '../add-card/add-card.component';
 import { AddFoulComponent } from '../add-foul/add-foul.component';
-import {
-  trigger,
-  transition,
-  style,
-  animate,
-  state,
-} from '@angular/animations';
+import { trigger, transition, style, animate } from '@angular/animations';
 import { CommonModule } from '@angular/common';
+import { MatchStateService } from '@app/services/global_states/match-state.service';
+import { Team } from '@app/models/team';
+import { TeamPlayerWithDetails } from '@app/models/team-player';
+import { Goal } from '@app/models/goal';
+import { MatchPlayerService } from '@app/services/api_services/match-player.service';
+import { MatchPlayerWithDetails } from '@app/models/matchPlayer';
 
 type FormType = 'GOAL' | 'CARD' | 'FOUL' | null;
 
@@ -33,10 +30,55 @@ type FormType = 'GOAL' | 'CARD' | 'FOUL' | null;
         animate('0.3s ease-in', style({ opacity: 0, maxHeight: '0px' })),
       ]),
     ]),
+    trigger('starAnimation', [
+      transition(':enter', [
+        style({ opacity: 0, transform: 'scale(0)' }),
+        animate('0.2s ease-in', style({ opacity: 1, transform: 'scale(1)' })),
+      ]),
+      transition(':leave', [
+        style({ opacity: 1, transform: 'scale(1)' }),
+        animate('0s ease-in', style({ opacity: 0, transform: 'scale(0)' })),
+      ]),
+    ]),
   ],
 })
 export class MatchEditComponent {
   public wichFormIsActive = signal<FormType>(null);
+  public localTeam: Team | null = null;
+  public visitorTeam: Team | null = null;
+  public localPlayers: TeamPlayerWithDetails[] = [];
+  public visitorPlayers: TeamPlayerWithDetails[] = [];
+  public goals: Goal[] = [];
+  public localGoals: Goal[] = [];
+  public visitorGoals: Goal[] = [];
+  public matchPlayers: MatchPlayerWithDetails[] = [];
+  public matchPlayersIds: number[] = [];
+
+  private _matchState = inject(MatchStateService);
+  private _matchPlayerService = inject(MatchPlayerService);
+
+  constructor() {
+    effect(() => {
+      const matchData = this._matchState.match();
+      if (matchData) {
+        this.localTeam = matchData.localTeam;
+        this.visitorTeam = matchData.visitorTeam;
+        this.localPlayers = matchData.localPlayers;
+        this.visitorPlayers = matchData.visitorPlayers;
+        this.goals = matchData.goals;
+        this.localGoals = this.goals.filter(
+          (goal) => goal.team_id === this.localTeam?.id_team
+        );
+        this.visitorGoals = this.goals.filter(
+          (goal) => goal.team_id === this.visitorTeam?.id_team
+        );
+        this.matchPlayers = matchData.matchPlayers;
+        this.matchPlayersIds = matchData.matchPlayers.map(
+          (matchPlayer) => matchPlayer.player_id
+        );
+      }
+    });
+  }
 
   setFormType(formType: FormType): string | null {
     if (this.wichFormIsActive() === formType) {
@@ -62,5 +104,64 @@ export class MatchEditComponent {
       };
     }
     return {};
+  }
+
+  onTogglePlayer(player: TeamPlayerWithDetails) {
+    if (this.matchPlayersIds.includes(player.player_id)) {
+      this.deleteMatchPlayer(player);
+      return;
+    }
+    this.createMatchPlayer(player);
+  }
+
+  private deleteMatchPlayer(player: TeamPlayerWithDetails) {
+    const matchPlayer = this._matchState
+      .match()
+      ?.matchPlayers.find(
+        (matchPlayer) => matchPlayer.player_id === player.player_id
+      );
+    console.log(matchPlayer);
+    if (!matchPlayer?.id_match_player) return;
+    this._matchPlayerService
+      .deleteMatchPlayer(matchPlayer.id_match_player)
+      .subscribe({
+        next: (res) => {
+          this._matchState.updateMatch({
+            ...this._matchState.match(),
+            matchPlayers: this._matchState
+              .match()
+              ?.matchPlayers.filter(
+                (matchPlayer) => matchPlayer.player_id !== player.player_id
+              ),
+          });
+        },
+        error: (err) => {
+          console.log(err);
+        },
+      });
+    return;
+  }
+
+  private createMatchPlayer(player: TeamPlayerWithDetails) {
+    this._matchPlayerService
+      .createMatchPlayer({
+        match_id: Number(this._matchState.match()?.match.id_match),
+        player_id: player.player_id,
+        team_id: player.team_id,
+        user_id: Number(this._matchState.match()?.match.user_id),
+        team_player_id: player.id_team_player,
+      })
+      .subscribe({
+        next: (res) => {
+          const matchPlayers = this._matchState.match()?.matchPlayers || [];
+          this._matchState.updateMatch({
+            ...this._matchState.match(),
+            matchPlayers: [...matchPlayers, res.data],
+          });
+        },
+        error: (err) => {
+          console.log(err);
+        },
+      });
   }
 }
