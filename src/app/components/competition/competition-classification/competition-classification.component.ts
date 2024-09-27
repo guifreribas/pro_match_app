5f2b228088b4bcd07dabf3cea0b8f10a6d2a12a9
Guifré Puig Ribas <guifreribas@gmail.com>
2024-09-27 21:38:26 +0200

    feat: add CompetitionClassification component with HTML, SCSS, and TypeScript files for displaying competition standings

---
+++ src/app/components/competition/competition-classification/competition-classification.component.ts
@@ -0,0 +1,22 @@
+import { Component, inject, Input, WritableSignal } from '@angular/core';
+import { config } from '@app/config/config';
+import { CompetitionWithDetails } from '@app/models/competition';
+import { StandingsStateService } from '@app/services/global_states/standings-state.service';
+
+@Component({
+  selector: 'app-competition-classification',
+  standalone: true,
+  imports: [],
+  templateUrl: './competition-classification.component.html',
+  styleUrl: './competition-classification.component.scss',
+})
+export class CompetitionClassificationComponent {
+  @Input() competition!: WritableSignal<CompetitionWithDetails | null>;
+
+  public imgUrl = config.IMG_URL;
+
+  private _standingsState = inject(StandingsStateService);
+  public standings = this._standingsState.standings;
+
+  constructor() {}
+}
