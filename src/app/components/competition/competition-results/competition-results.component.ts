d4e41e32ceda228ad77e931c919105d8a8ec3789
Guifré Puig Ribas <guifreribas@gmail.com>
2024-09-28 16:46:44 +0200

    feat: add competition results component with display logic for finished matches and integration with match state service

---
+++ src/app/components/competition/competition-results/competition-results.component.ts
@@ -0,0 +1,34 @@
+import {
+  Component,
+  effect,
+  inject,
+  Input,
+  WritableSignal,
+} from '@angular/core';
+import {
+  MatchCompletedData,
+  MatchStateService,
+} from '@app/services/global_states/match-state.service';
+import { CompetitionMatchResultComponent } from '../competition-match-result/competition-match-result.component';
+import { MatchWithDetails } from '@app/models/match';
+
+@Component({
+  selector: 'app-competition-results',
+  standalone: true,
+  imports: [CompetitionMatchResultComponent],
+  templateUrl: './competition-results.component.html',
+  styleUrl: './competition-results.component.scss',
+})
+export class CompetitionResultsComponent {
+  @Input() matches!: MatchWithDetails[];
+
+  public match: MatchCompletedData | null = null;
+  private _matchState = inject(MatchStateService);
+
+  constructor() {
+    effect(() => {
+      const match = this._matchState.match();
+      this.match = match;
+    });
+  }
+}
