fc280dd3e101fd7188dacaf9f4272a4da2a4f6fb
Guifré Puig Ribas <guifreribas@gmail.com>
2024-10-02 23:29:12 +0200

    feat: add GlobalSpinnerComponent with template and styles for improved loading feedback in the app

---
+++ src/app/components/atom/global-spinner/global-spinner.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-global-spinner',
+  standalone: true,
+  imports: [],
+  templateUrl: './global-spinner.component.html',
+  styleUrl: './global-spinner.component.scss'
+})
+export class GlobalSpinnerComponent {
+
+}
