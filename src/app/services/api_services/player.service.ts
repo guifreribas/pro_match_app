207386cac7ac900174357ac410045d10dbe59455
Guifré Puig Ribas <guifreribas@gmail.com>
2024-10-02 23:30:10 +0200

    feat: refactor PlayerService to use static apiUrl and improve getPlayers method with optional parameters for better flexibility

--- src/app/services/api_services/player.service.ts
+++ src/app/services/api_services/player.service.ts
@@ -5,47 +5,63 @@ import { Observable } from 'rxjs';
 import { getAllResponse, getOneResponse, postResponse } from '../../models/api';
 import { Player, PlayerCreateResponse } from '../../models/player';
 import { urlParser } from '@app/utils/utils';
+import { buildHttpParams } from '@app/utils/http-utils';
 
 interface GetPlayersParams {
-  q?: string;
-  page?: string;
-  user_id?: number;
+  q: string;
+  page: string;
+  user_id: number;
+  team_id: number;
 }
 
 @Injectable({
   providedIn: 'root',
 })
 export class PlayerService {
-  public apiUrl = `${config.apiUrl}/players`;
+  public static readonly apiUrl = `${config.apiUrl}/players`;
   private genericService = inject(GenericApiService);
 
   constructor() {}
 
-  getPlayers(params?: GetPlayersParams): Observable<getAllResponse<Player>> {
-    const url = urlParser(params, this.apiUrl);
-    return this.genericService.getAll<getAllResponse<Player>>(url);
+  getPlayers(
+    params?: Partial<GetPlayersParams>
+  ): Observable<getAllResponse<Player>> {
+    const httpParams = buildHttpParams(params);
+    return this.genericService.getAll<getAllResponse<Player>>(
+      PlayerService.apiUrl,
+      { params: httpParams }
+    );
   }
 
   getPlayer(id: number): Observable<getOneResponse<Player>> {
-    return this.genericService.getOne<getOneResponse<Player>>(this.apiUrl, id);
+    return this.genericService.getOne<getOneResponse<Player>>(
+      PlayerService.apiUrl,
+      id
+    );
   }
 
   createPlayer(player: Player): Observable<postResponse<PlayerCreateResponse>> {
     return this.genericService.create<
       Player,
       postResponse<PlayerCreateResponse>
-    >(this.apiUrl, player);
+    >(PlayerService.apiUrl, player);
   }
 
-  updatePlayer(player: Player, id: number): Observable<getAllResponse<Player>> {
-    return this.genericService.update<Player, getAllResponse<Player>>(
-      this.apiUrl,
+  updatePlayer(
+    player: Partial<Player>,
+    id: number
+  ): Observable<getAllResponse<Player>> {
+    return this.genericService.update<Partial<Player>, getAllResponse<Player>>(
+      PlayerService.apiUrl,
       id,
       player
     );
   }
 
   deletePlayer(id: number): Observable<getAllResponse<Player>> {
-    return this.genericService.delete<getAllResponse<Player>>(this.apiUrl, id);
+    return this.genericService.delete<getAllResponse<Player>>(
+      PlayerService.apiUrl,
+      id
+    );
   }
 }
