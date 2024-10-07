4bd956b762c595080325bb0143b042fa3b0a22db
Guifré Puig Ribas <guifreribas@gmail.com>
2024-10-07 21:43:23 +0200

    feat: refactor TeamService to use static apiUrl, improving consistency and reducing instance variable reliance in API calls

--- src/app/services/api_services/team.service.ts
+++ src/app/services/api_services/team.service.ts
@@ -14,23 +14,26 @@ import { urlParser } from '@app/utils/utils';
   providedIn: 'root',
 })
 export class TeamService {
-  public apiUrl = `${config.apiUrl}/teams`;
+  static readonly apiUrl = `${config.apiUrl}/teams`;
   private genericService = inject(GenericApiService);
 
   constructor() {}
 
   getTeams(params?: Partial<GetTeamsParams>): Observable<getAllResponse<Team>> {
-    const url = urlParser(params, this.apiUrl);
+    const url = urlParser(params, TeamService.apiUrl);
     return this.genericService.getAll<getAllResponse<Team>>(url);
   }
 
   getTeam(id: number): Observable<getOneResponse<Team>> {
-    return this.genericService.getOne<getOneResponse<Team>>(this.apiUrl, id);
+    return this.genericService.getOne<getOneResponse<Team>>(
+      TeamService.apiUrl,
+      id
+    );
   }
 
   createTeam(team: Team): Observable<getAllResponse<Team>> {
     return this.genericService.create<Team, getAllResponse<Team>>(
-      this.apiUrl,
+      TeamService.apiUrl,
       team
     );
   }
@@ -40,13 +43,16 @@ export class TeamService {
     id: number
   ): Observable<updateResponse<Team>> {
     return this.genericService.update<Partial<Team>, updateResponse<Team>>(
-      this.apiUrl,
+      TeamService.apiUrl,
       id,
       team
     );
   }
 
   deleteTeam(id: number): Observable<getAllResponse<Team>> {
-    return this.genericService.delete<getAllResponse<Team>>(this.apiUrl, id);
+    return this.genericService.delete<getAllResponse<Team>>(
+      TeamService.apiUrl,
+      id
+    );
   }
 }
