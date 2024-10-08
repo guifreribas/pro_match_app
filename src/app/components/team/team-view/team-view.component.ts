2f86d07eb0a04f690fd860c92f18b1dfb167e5b2
Guifré Puig Ribas <guifreribas@gmail.com>
2024-10-08 10:18:05 +0200

    feat: refactor team-view with new layout, added background image, and incorporated TeamPlayerStateService for player management

--- src/app/components/team/team-view/team-view.component.ts
+++ src/app/components/team/team-view/team-view.component.ts
@@ -1,5 +1,6 @@
 import {
   Component,
+  effect,
   ElementRef,
   inject,
   Input,
@@ -41,6 +42,7 @@ import { AfterViewInit } from '@angular/core';
 import { TeamPlayer } from '../../../models/team-player';
 import { GlobalModalService } from '@app/services/global-modal.service';
 import { TeamStateService } from '@app/services/global_states/team-state.service';
+import { TeamPlayerStateService } from '@app/services/global_states/team-player-state.service';
 
 @Component({
   selector: 'app-team-view',
@@ -58,16 +60,33 @@ export class TeamViewComponent implements OnInit {
   @Input() teamViewState!: WritableSignal<TeamViewState>;
 
   public imgUrl: string = config.IMG_URL;
-  public teamPlayers = signal<TeamPlayerWithDetails[]>([]);
+  public avatarImg: string = '';
+  public teamPlayers = TeamPlayerStateService.teamPlayers;
+  // public teamPlayers = signal<TeamPlayerWithDetails[]>([]);
   public searchedPlayers = signal<Player[] | null>(null);
   public playerSearchInput: FormControl = new FormControl('');
   public isPlayerCreatedSuccessfully = false;
   public playerForms: { [key: string]: FormGroup } = {};
   public team = TeamStateService.activeTeam;
 
+  private _teamPlayerState = inject(TeamPlayerStateService);
+
+  constructor() {
+    effect(() => {
+      const teamPlayers = TeamPlayerStateService.teamPlayers();
+      if (teamPlayers) {
+        this.imgUrl = config.IMG_URL;
+      }
+    });
+  }
+
   ngOnInit(): void {
     setTimeout(() => {
       initFlowbite();
     }, 100);
+
+    console.log('IMAGE AVATAR URL', this.imgUrl, this.teamPlayers());
+
+    // this.teamPlayers.set(TeamPlayerStateService.teamPlayers());
   }
 }
