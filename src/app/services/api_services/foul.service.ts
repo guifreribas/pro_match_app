4ce427b34bd20daf61eb4f4c593ddc77341880b9
Guifré Puig Ribas <guifreribas@gmail.com>
2024-10-09 11:28:35 +0200

    feat: refactor FoulService to use static apiUrl for consistency in API calls and improved parameter handling in getFouls method

--- src/app/services/api_services/foul.service.ts
+++ src/app/services/api_services/foul.service.ts
@@ -4,40 +4,61 @@ import { GenericApiService } from './generic-api.service';
 import { Observable } from 'rxjs';
 import { getAllResponse } from '../../models/api';
 import { Foul } from '../../models/foul';
+import { buildHttpParams } from '@app/utils/http-utils';
+
+interface GetFoulsParams {
+  q: string;
+  page: string;
+  user_id: number;
+  limit: number;
+  match_id: number;
+  team_id: number;
+  competition_id: number;
+}
 
 @Injectable({
   providedIn: 'root',
 })
 export class FoulService {
-  private apiUrl = `${config.apiUrl}/fouls`;
+  private static readonly apiUrl = `${config.apiUrl}/fouls`;
   private genericService = inject(GenericApiService);
 
   constructor() {}
 
-  getFouls(): Observable<getAllResponse<Foul>> {
-    return this.genericService.getAll<getAllResponse<Foul>>(this.apiUrl);
+  getFouls(params?: Partial<GetFoulsParams>): Observable<getAllResponse<Foul>> {
+    const HttpParams = buildHttpParams(params);
+    return this.genericService.getAll<getAllResponse<Foul>>(
+      FoulService.apiUrl,
+      { params: HttpParams }
+    );
   }
 
   getFoul(id: number): Observable<getAllResponse<Foul>> {
-    return this.genericService.getOne<getAllResponse<Foul>>(this.apiUrl, id);
+    return this.genericService.getOne<getAllResponse<Foul>>(
+      FoulService.apiUrl,
+      id
+    );
   }
 
   createFoul(foul: Foul): Observable<getAllResponse<Foul>> {
     return this.genericService.create<Foul, getAllResponse<Foul>>(
-      this.apiUrl,
+      FoulService.apiUrl,
       foul
     );
   }
 
   updateFoul(foul: Foul, id: number): Observable<getAllResponse<Foul>> {
     return this.genericService.update<Foul, getAllResponse<Foul>>(
-      this.apiUrl,
+      FoulService.apiUrl,
       id,
       foul
     );
   }
 
   deleteFoul(id: number): Observable<getAllResponse<Foul>> {
-    return this.genericService.delete<getAllResponse<Foul>>(this.apiUrl, id);
+    return this.genericService.delete<getAllResponse<Foul>>(
+      FoulService.apiUrl,
+      id
+    );
   }
 }
