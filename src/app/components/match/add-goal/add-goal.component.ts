6601c41660252b683c70ea14485299c2fde0a313
Guifré Puig Ribas <guifreribas@gmail.com>
2024-10-10 00:22:13 +0200

    feat: rename onSubmit to onAddGoal, refactor goal creation with improved state management and player selection logic updates

--- src/app/components/match/add-goal/add-goal.component.ts
+++ src/app/components/match/add-goal/add-goal.component.ts
@@ -4,6 +4,7 @@ import {
   effect,
   inject,
   Input,
+  OnInit,
   WritableSignal,
 } from '@angular/core';
 import {
@@ -12,7 +13,8 @@ import {
   ReactiveFormsModule,
   Validators,
 } from '@angular/forms';
-import { Goal } from '@app/models/goal';
+import { Goal, GoalPart } from '@app/models/goal';
+import { MatchWithDetails } from '@app/models/match';
 import { Team } from '@app/models/team';
 import { TeamPlayerWithDetails } from '@app/models/team-player';
 import { GoalService } from '@app/services/api_services/goal.service';
@@ -23,7 +25,9 @@ import {
   MatchStateService,
 } from '@app/services/global_states/match-state.service';
 import { UserStateService } from '@app/services/global_states/user-state.service';
-import { firstValueFrom } from 'rxjs';
+import { firstValueFrom, Subject, takeUntil } from 'rxjs';
+import { VisitorTeam } from '../../../models/match';
+import { SpinnerService } from '@app/services/spinner.service';
 
 type FormType = 'GOAL' | 'CARD' | 'FOUL' | null;
 
@@ -39,7 +43,7 @@ interface UpdateStandingsParams {
   templateUrl: './add-goal.component.html',
   styleUrl: './add-goal.component.scss',
 })
-export class AddGoalComponent {
+export class AddGoalComponent implements OnInit {
   @Input() whichFormIsActive!: WritableSignal<FormType>;
   public goalForm = new FormGroup({
     team: new FormControl('', [Validators.required]),
@@ -59,135 +63,205 @@ export class AddGoalComponent {
   private _goalService = inject(GoalService);
   private _matchState = inject(MatchStateService);
   private _standingsService = inject(StandingsService);
-  private _globalModalService = inject(GlobalModalService);
+  private _globalModal = inject(GlobalModalService);
+  private _spinner = inject(SpinnerService);
+  private destroy$ = new Subject<void>();
 
   constructor() {
-    effect(() => {
-      const matchData = this._matchState.match();
-      if (matchData) {
-        this.localTeam = matchData.localTeam;
-        this.visitorTeam = matchData.visitorTeam;
-        this.localPlayers = matchData.localPlayers;
-        this.visitorPlayers = matchData.visitorPlayers;
-        this.goals = matchData.goals;
+    this.setupEffects();
+  }
 
+  private setupEffects() {
+    effect(() => {
+      const { localTeam, visitorTeam } = this._matchState;
+      if (localTeam) {
+        this.localTeam = localTeam();
         this.goalForm.controls.team.setValue(String(this.localTeam?.id_team));
+      }
+      if (visitorTeam) {
+        this.visitorTeam = visitorTeam();
       }
     });
 
-    this.goalForm.controls.team.valueChanges.subscribe((teamId) => {
-      const localTeamId = String(this.localTeam?.id_team);
-      this.whatTeamIsSelected =
-        teamId === localTeamId ? 'LOCAL_TEAM' : 'VISITOR_TEAM';
-      this.goalForm.controls.player.setValue('');
+    effect(() => {
+      const localPlayers = this._matchState.localPlayers();
+      const visitorPlayers = this._matchState.visitorPlayers();
+      if (localPlayers) this.localPlayers = localPlayers;
+      if (visitorPlayers) this.visitorPlayers = visitorPlayers;
+    });
+
+    effect(() => {
+      const goals = this._matchState.goals();
+      if (goals) this.goals = goals;
     });
   }
 
-  async onSubmit(e: Event) {
-    e.preventDefault();
+  ngOnInit(): void {
+    this.goalForm.controls.team.valueChanges
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((teamId) => {
+        const localTeamId = String(this.localTeam?.id_team);
+        this.whatTeamIsSelected =
+          teamId === localTeamId ? 'LOCAL_TEAM' : 'VISITOR_TEAM';
+        this.goalForm.controls.player.setValue('');
+      });
+  }
 
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+
+  private _createGoalBody(): Goal {
     const { minute, part, player, team } = this.goalForm.value;
     const match = this._matchState.match();
-    const matchGoals = match?.goals || [];
-    const localTeamId = this.localTeam?.id_team ?? 0;
-    const visitorTeamId = this.visitorTeam?.id_team ?? 0;
-    const hasLocalTeamScored = Number(team) === localTeamId;
+    const match_id = match?.id_match ?? 0;
+    const competition_id = match?.competition.id_competition ?? 0;
+    const user_id = this._userState.me()?.id_user ?? 0;
+    const goalBody: Goal = {
+      minute: Number(minute),
+      part: part as GoalPart,
+      player_id: Number(player),
+      team_id: Number(team),
+      match_id,
+      user_id,
+      competition_id,
+    };
+
+    return goalBody;
+  }
+
+  async onAddGoal(e: Event) {
+    e.preventDefault();
+
+    const goalBody = this._createGoalBody();
+    if (this.goalForm.invalid) {
+      this._globalModal.openModal('Ha ocurrido un error', 'Intenta de nuevo');
+      return;
+    }
 
     try {
       //Create goal
+      this._spinner.setLoading(true);
       const createGoalResponse = await firstValueFrom(
-        this._goalService.createGoal({
-          minute: minute,
-          part: part,
-          player_id: Number(player),
-          team_id: Number(team),
-          match_id: match?.match.id_match,
-          user_id: this._userState.me()?.id_user,
-          competition_id: match?.match.competition.id_competition,
-        })
+        this._goalService.createGoal(goalBody)
       );
 
-      this._matchState.updateMatch({
-        ...match,
-        goals: [...matchGoals, createGoalResponse.data],
+      this._matchState.goals.update((prev) => {
+        if (!prev) return prev;
+        return [...prev, createGoalResponse.data];
       });
 
-      const calculateGoals = (teamId: number, hasScored: boolean) => {
-        const previousGoals = matchGoals.filter(
-          (goal) => goal.team_id === teamId
-        );
-        console.log({ previousGoals });
-        return hasScored ? previousGoals.length + 1 : previousGoals.length;
-      };
-      const localTeamGoals = calculateGoals(localTeamId, hasLocalTeamScored);
-      const visitorTeamGoals = calculateGoals(
-        visitorTeamId,
-        hasLocalTeamScored
-      );
+      // await this.updateTeamStandings(
+      //   this.localTeam!.id_team!,
+      //   this.visitorTeam!.id_team!,
+      //   goalBody
+      // );
 
-      const competitionId = match?.match.competition.id_competition;
-      if (competitionId && localTeamId && visitorTeamId) {
-        //Get Id and Update Local Team Standings
-        const standingsLocalTeam = await this.getStandings(localTeamId, match);
-        console.log('STANDINGS LOCAL TEAM', standingsLocalTeam);
-        const standingLocalTeam = standingsLocalTeam.data.items[0];
-        const standingIdLocalTeam = standingLocalTeam.id_standings;
-        if (!standingIdLocalTeam) return;
-        await firstValueFrom(
-          this._standingsService.updateStanding(
-            { goals_for: localTeamGoals, goals_against: visitorTeamGoals },
-            standingIdLocalTeam
-          )
-        );
-        //Get Id and Update Visitor Team Standings
-        const standingsVisitorTeam = await this.getStandings(
-          visitorTeamId,
-          match
-        );
-        const standingVisitorTeam = standingsVisitorTeam.data.items[0];
-        const standingIdVisitorTeam = standingVisitorTeam.id_standings;
-        if (!standingIdVisitorTeam) return;
-        await firstValueFrom(
-          this._standingsService.updateStanding(
-            { goals_for: visitorTeamGoals, goals_against: localTeamGoals },
-            standingIdVisitorTeam
-          )
-        );
-      }
+      const playerName =
+        this.getNameOfPlayer(Number(this.goalForm.value.player)) ??
+        'Un jugador';
 
-      console.log({ localTeamGoals, visitorTeamGoals });
-
-      const playerName = this.getNameOfPlayer(Number(this.goalForm.value));
-      this._globalModalService.openModal(
+      this._globalModal.openModal(
         'Gol en el campo!',
-        ''
-        // `Ha marcado ${playerName}!`
+        `Ha marcado ${playerName}!`
       );
     } catch (error) {
       console.error(error);
-      this._globalModalService.openModal(
+      this._globalModal.openModal(
         'No ha habido gol!',
         'Ha ocurrido un error. Prueba de nuevo!'
       );
+    } finally {
+      this._spinner.setLoading(false);
     }
   }
 
-  async getStandings(teamId: number, match: MatchCompletedData) {
+  private async updateTeamStandings(
+    localTeamId: number,
+    visitorTeamId: number,
+    goalBody: Goal
+  ) {
+    const matchGoals = this._matchState.goals() || [];
+    const hasLocalTeamScored = goalBody.team_id === localTeamId;
+    const hasVisitorTeamScored = goalBody.team_id === visitorTeamId;
+
+    const localTeamGoals = this._calculateGoals(
+      localTeamId,
+      matchGoals,
+      hasLocalTeamScored
+    );
+    const visitorTeamGoals = this._calculateGoals(
+      visitorTeamId,
+      matchGoals,
+      hasVisitorTeamScored
+    );
+
+    const match = this._matchState.match();
+
+    const standingsLocalTeams = await this._getStandings(localTeamId);
+    const standingVisitorTeams = await this._getStandings(visitorTeamId);
+    const standingLocalTeam = standingsLocalTeams.data.items[0];
+    const visitorLocalTeam = standingVisitorTeams.data.items[0];
+
+    await this._updateStandings(
+      standingLocalTeam.id_standings,
+      localTeamGoals,
+      visitorTeamGoals
+    );
+    await this._updateStandings(
+      visitorLocalTeam.id_standings,
+      visitorTeamGoals,
+      localTeamGoals
+    );
+  }
+
+  private _calculateGoals = (
+    teamId: number,
+    goals: Goal[],
+    scoredByTeam: boolean
+  ) => {
+    return (
+      goals.filter((goal) => goal.team_id === teamId).length +
+      (scoredByTeam ? 1 : 0)
+    );
+  };
+
+  private async _getStandings(teamId: number) {
+    const match = this._matchState.match();
     return await firstValueFrom(
       this._standingsService.getStandings({
-        competition_id: match?.match.competition.id_competition,
+        competition_id: match?.competition.id_competition,
+        user_id: this._userState.me()?.id_user,
         team_id: teamId,
       })
     );
   }
 
-  getNameOfPlayer(playerId: number) {
+  private async _updateStandings(
+    standingId: number,
+    goalsFor: number,
+    goalsAgainst: number
+  ) {
+    await firstValueFrom(
+      this._standingsService.updateStanding(
+        {
+          goals_for: goalsFor,
+          goals_against: goalsAgainst,
+        },
+        standingId
+      )
+    );
+  }
+
+  getNameOfPlayer(playerId: number | null) {
+    if (!playerId) return 'Un jugador';
     const allMatchPlayers = [...this.localPlayers, ...this.visitorPlayers];
     const player = allMatchPlayers.find((player) => {
       console.log({ playerId, playerId2: player.player_id });
       return player.player_id === playerId;
     });
-    return player?.player?.name;
+    return `${player?.player?.name} ${player?.player?.last_name}`;
   }
 
   onCancel(e: Event) {
