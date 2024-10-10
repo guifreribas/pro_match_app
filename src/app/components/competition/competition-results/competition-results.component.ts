3eb51a8ef934b4f909e9d7cf33a756329a93b587
Guifré Puig Ribas <guifreribas@gmail.com>
2024-10-10 09:11:35 +0200

    feat: refactor competition results to improve match retrieval logic and correctly display finished matches state handling

--- src/app/components/competition/competition-results/competition-results.component.ts
+++ src/app/components/competition/competition-results/competition-results.component.ts
@@ -3,6 +3,7 @@ import {
   effect,
   inject,
   Input,
+  signal,
   WritableSignal,
 } from '@angular/core';
 import {
@@ -11,6 +12,10 @@ import {
 } from '@app/services/global_states/match-state.service';
 import { CompetitionMatchResultComponent } from '../competition-match-result/competition-match-result.component';
 import { MatchWithDetails } from '@app/models/match';
+import { MatchService } from '@app/services/api_services/match.service';
+import { firstValueFrom } from 'rxjs';
+import { CompetitionStateService } from '@app/services/global_states/competition-state.service';
+import { CompetitionWithDetails } from '@app/models/competition';
 
 @Component({
   selector: 'app-competition-results',
@@ -24,18 +29,40 @@ export class CompetitionResultsComponent {
 
   public match: MatchWithDetails | null = null;
   public hasFinishedMatches = false;
+  public competition: CompetitionWithDetails | null = null;
+  public matchesPlayed = signal<MatchWithDetails[]>([]);
+
   private _matchState = inject(MatchStateService);
+  private _competitionState = inject(CompetitionStateService);
+  private _matchService = inject(MatchService);
 
   constructor() {
     effect(() => {
-      const match = this._matchState.match();
-      this.match = match;
+      const competition = this._competitionState.competitions();
+      if (competition) {
+        this.competition = competition;
+        this._getMatches(
+          competition.competitionCategory.competition_category_id
+        );
+      }
     });
   }
 
   ngOnInit(): void {
-    this.hasFinishedMatches = this.matches.some(
-      (match) => match.status === 'FINISHED'
+    console.log('MATCH STATE', this.matches);
+    // this.hasFinishedMatches = this.matches.some(
+    //   (match) => match.status === 'FINISHED'
+    // );
+  }
+
+  private async _getMatches(competition_category_id: number) {
+    const matches = await firstValueFrom(
+      this._matchService.getMatches({
+        competition_category_id,
+        limit: 100,
+      })
     );
+    console.log('MATCHES RESPOSNE: ', matches);
+    this.matchesPlayed.set(matches.data.items);
   }
 }
